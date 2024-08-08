f969f1b26f87943ff8b9f9de2723c8b5d73ffedd
wwssllxx <749877359@qq.com>
2024-08-08 13:58:24 +1000

    add survey feedback empty page

---
+++ components/surveyfeedback-page.tsx
@@ -0,0 +1,15 @@
+import { usePage } from "@/lib/usePage"
+import { Button } from "./ui/button"
+import { useState } from "react";
+import { RadarChart } from "./ui/chart";
+
+
+export const FeedbackPage = ({ data }: { data: any }) => {
+    return (
+        <>
+            <div className="w-full bg-[#fafafc]">
+                
+            </div>
+        </>
+    )
+}

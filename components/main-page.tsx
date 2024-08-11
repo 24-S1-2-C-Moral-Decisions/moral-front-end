33156300eb73e54c4538fadbab02af8c9e284ba8
lzh <lzh13298370379@126.com>
2024-08-11 23:40:41 +1000

    situation animation

--- components/main-page.tsx
+++ components/main-page.tsx
@@ -1,12 +1,20 @@
+import * as React from "react";
 import { usePage } from "@/lib/usePage"
-import { Button } from "./ui/button"
-import { useState } from "react";
 import { cn } from "@/lib/utils";
-
+import { Button } from "./ui/button"
 
 export const MainPage = ({ data }: { data: any }) => {
     const { setPage } = usePage();
-    const [close, setClose] = useState("false");
+
+    const [close, setClose] = React.useState("false");
+    const [animation, setAnimation] = React.useState(true);
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setAnimation(false);
+        }, 2000)
+    }, [])
+
     return (
         <div className={cn("relative flex flex-col h-full w-full overflow-hidden", { "animate-shrink": close === "true" })}>
             <div className="absolute z-[-2] bg-[#2B2B2B] w-full h-full clip-circle-3-30-35 animate-expand-circle" />
@@ -17,11 +25,18 @@ export const MainPage = ({ data }: { data: any }) => {
 
             <div className="flex w-full flex-grow items-center ">
                 <div className="pl-20 pb-20 flex flex-col w-[50%]">
-                    <h1 className="font-semibold text-6xl whitespace-pre-wrap animate-text-fade-to-white">
-                        {data["main-header"]}
-                        <span className="text-[#4767E7]">
-                            {data["main-game"]}
+                    <h1 className="relative flex flex-col font-semibold text-6xl whitespace-pre-wrap animate-text-fade-to-white">
+                        <span className="leading-normal">
+                            Judge a Tricky
                         </span>
+                        <div>
+                            <span className="flex">
+                                Moral
+                                <div className={`animate-typing-effect whitespace-nowrap overflow-hidden ${animation ? "border-r-2" : ""} text-[#4767E7] ml-5`}>
+                                    Situation.
+                                </div>
+                            </span>
+                        </div>
                     </h1>
                     <p className="mt-5 animate-text-fade-to-white">
                         {data["main-text"]}
@@ -42,8 +57,8 @@ export const MainPage = ({ data }: { data: any }) => {
                 </div>
 
                 <div className="relative flex w-[50%] justify-center items-center">
-                    <div className="absolute w-[100px] h-[100px] p-5 bg-gray-200 rounded-full animate-expand-img-bg"/>
-                        <img className="animate-expand-img scale-75" src="/imgs/Analytics.png" alt="Analytics" />
+                    <div className="absolute w-[100px] h-[100px] p-5 bg-gray-200 rounded-full animate-expand-img-bg" />
+                    <img className="animate-expand-img scale-75" src="/imgs/Analytics.png" alt="Analytics" />
                 </div>
 
             </div>

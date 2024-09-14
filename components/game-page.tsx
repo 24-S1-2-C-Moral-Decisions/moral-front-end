c42a1ca03bb9f36be31bb42d6a7014705057de2c
lzh <lzh13298370379@126.com>
2024-09-14 13:54:16 +1000

    implement lastest UI design

--- components/game-page.tsx
+++ components/game-page.tsx
@@ -83,13 +83,87 @@ export const GamePage = ({ data }: { data: any }): JSX.Element => {
     };
 
     React.useEffect(() => updateAnimation(), [currentDialogInfoIndex]);
-    
+
     return (
         <div className={cn("flex flex-col h-full w-full transition-opacity opacity-0", { "animate-fade-in": page === "game" })} >
-            <div className="w-full pt-10 pl-20  ">
-                <img src="../imgs/Logo-3.svg" alt="Logo" width="240px" height="61px" />
+            <div className="flex ">
+                <div className="w-full pt-10 pl-20">
+                    <img src="../imgs/Logo-3.svg" alt="Logo" width="240px" height="61px" />
+                </div>
+
+                <div className="pt-20 pr-20 flex justify-center">
+                    <img src="../imgs/close-btn.svg" alt="close-btn" width="16px" height="16px" />
+                </div>
             </div>
-            <div className="flex justify-center mt-10">
+            <div className="flex flex-col justify-center items-center mt-2 ">
+                <div className="w-[75%] flex flex-col justify-center items-center gap-y-5">
+                    <p className="text-center text-black text-center font-kanit text-xl font-semibold leading-normal ">
+                        {data["game-text"]}
+                    </p>
+                    <div className="relative flex items-center justify-center">
+                        <img className="min-w-[756px] h-[482px] " src="../imgs/game-bg1.png" alt="scene" />
+                        {talkDialog[currentDialogInfoIndex].gender === "male" && (
+                            <div className="absolute top-[48px] right-[148px]  w-[20%]">
+                                <img
+                                    className={`animate-${animationType}`}
+                                    src={talkDialog[currentDialogInfoIndex].src}
+                                    alt="maleDialog"
+                                />
+                            </div>
+                        )}
+                        {talkDialog[currentDialogInfoIndex].gender === "female" && (
+                            <div className="absolute top-[36px] left-[120px]  w-[20%]">
+                                <img
+                                    className={`animate-${animationType}`}
+                                    src={talkDialog[currentDialogInfoIndex].src}
+                                    alt="femaleDialog"
+                                />
+                            </div>
+                        )}
+                        {(currentDialogInfoIndex === 3 && isAnswerDialogVisible) ?
+                            answer === true ?
+                                (
+                                    <div className="w-[20%] absolute top-[36px] left-[120px] flex items-center justify-center animate-fade-in">
+                                        <img className="animate-fade-in" src="../imgs/answer-yes-dialog.png" alt="answerYes" />
+                                    </div>
+                                ) :
+                                (
+                                    <div className="w-[20%] absolute top-[36px] left-[120px] flex items-center justify-center animate-fade-in w-[20%]">
+                                        <img className="animate-fade-in" src="../imgs/answer-no-dialog.png" alt="answerNo" />
+                                    </div>
+                                )
+                            : null
+                        }
+                        {isInteractiveDialogVisible && (
+                            <div className={`opacity: ${isInteractiveDialogVisible ? 1 : 0} absolute h-full w-full top-0 left-0 flex items-center justify-center animate-${isInteractiveDialogVisible ? "fade-in" : "fade-out"}`}>
+                                <div className="flex w-full h-full items-center justify-center flex-col">
+                                    <img className="h-full w-full" src="../imgs/interactive-dialog.png" alt="interactive" />
+                                    <div className="flex justify-center items-center absolute bottom-[14%]" >
+                                        <button
+                                            className="mr-[20px]"
+                                            onClick={() => buttonClick(true)}
+                                            onMouseEnter={() => setIsYesButtonHover(true)}
+                                            onMouseLeave={() => setIsYesButtonHover(false)}
+                                        >
+                                            <img src={isYesButtonHover ? "../imgs/yes-button-hover.png" : "../imgs/yes-button.png"} alt="yesButton" width="100px" height="60px" />
+                                        </button>
+                                        <button
+                                            className="ml-[20px]"
+                                            onClick={() => buttonClick(false)}
+                                            onMouseEnter={() => setIsNoButtonHover(true)}
+                                            onMouseLeave={() => setIsNoButtonHover(false)}
+                                        >
+                                            <img src={isNoButtonHover ? "../imgs/no-button-hover.png" : "../imgs/no-button.png"} alt="noButton" width="100px" height="60px" />
+                                        </button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+            {/* <div className="flex justify-center mt-10">
                 <div className="relative w-[50%] h-full min-w-[1305px]">
                     <p className="text-black text-center font-kanit text-xl font-semibold leading-normal ">
                         {data["game-text"]}
@@ -159,7 +233,7 @@ export const GamePage = ({ data }: { data: any }): JSX.Element => {
                         )}
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }

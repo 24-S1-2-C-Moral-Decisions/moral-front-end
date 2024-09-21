caa7a0ede75f3ef6ecb6d5203e9d6654bc3562af
Zehua Kong <u7693498@anu.edu.au>
2024-09-21 12:40:16 +1000

    Revert "Add: get survey url from env variable"
    This reverts commit 1d07b6b290eededca001e25bd92587b720e0c2f4.

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,7 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export function getSurveyURL(): string {
-  return process.env.NEXT_PUBLIC_SURVEY_URL || '/default-survey'
-}
